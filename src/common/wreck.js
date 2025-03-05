665e9c9c8cff5a7492189463b8210146d926a3b9
Donatas Bacius <bacius@gmail.com>
2025-03-05 21:52:02 +0000

    Use wreck

---
+++ src/common/wreck.js
@@ -0,0 +1,16 @@
+import Wreck from '@hapi/wreck'
+import { getTraceId } from '@defra/hapi-tracing'
+import { config } from './config.js'
+
+export const wreck = Wreck.defaults({
+  events: true,
+  timeout: 3000
+})
+
+wreck.events.on('preRequest', (uri) => {
+  const traceId = getTraceId()
+
+  if (traceId) {
+    uri.headers[config.get('tracing.header')] = traceId
+  }
+})
