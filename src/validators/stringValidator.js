494ce37a6fbe4b70696e5ca5093f12d71da5d7f0
Tej Powar <tej@powtech.co.uk>
2025-03-24 16:21:04 +0000

    introduce joi

--- src/validators/stringValidator.js
+++ src/validators/stringValidator.js
@@ -14,15 +14,30 @@ export const validateString = (fieldName, value, validations) => {
       });
   }
 
-  // if (validations.minLength !== undefined) {
-  //   schema = schema.min(validations.minLength)
-  //     .message(`${fieldName} shorter than the minimum length of ${validations.minLength} characters`);
-  // }
-  //
-  // if (validations.pattern !== undefined) {
-  //   schema = schema.pattern(new RegExp(validations.pattern))
-  //     .message(`${fieldName} must be in the format ${validations.pattern}`);
-  // }
+  if (validations.minLength !== undefined) {
+    schema = schema
+      .min(validations.minLength)
+      .required()
+      .messages({
+        "string.base": `${fieldName} must be a string`,
+        "string.empty": `${fieldName} cannot be empty`,
+        "string.min": `${fieldName} shorter than the minimum length of ${validations.minLength} characters`,
+      });
+  }
+
+  if (validations.pattern !== undefined) {
+    try {
+      const regex = new RegExp(validations.pattern);
+      schema = schema.pattern(regex).messages({
+        "string.pattern.base": `${fieldName} must be in the format ${validations.pattern}`,
+      });
+    } catch (error) {
+      return {
+        isValid: false,
+        error: `Invalid regular expression pattern: ${validations.pattern}`,
+      };
+    }
+  }
 
   const result = schema.validate(value);
 
--- src/validators/stringValidator.test.js
+++ src/validators/stringValidator.test.js
@@ -36,56 +36,67 @@ describe("validateString", () => {
     });
   });
 
-  // describe("minLength validation", () => {
-  //   it("should return error when string is less than minLength", () => {
-  //     const result = validateString("testField", "abc", {
-  //       minLength: 5,
-  //     });
-  //     assert.equal(result.isValid, false);
-  //     assert.equal(
-  //       result.error,
-  //       "testField shorter than the minimum length of 5 characters",
-  //     );
-  //   });
-  //   it("should pass when string is equal to minLength", () => {
-  //     const result = validateString("testField", "abcde", { minLength: 5 });
-  //     assert.equal(result.isValid, true);
-  //   });
-  //
-  //   it("should pass when string is greater than maxLength", () => {
-  //     const result = validateString("testField", "abcdef", { minLength: 5 });
-  //     assert.equal(result.isValid, true);
-  //   });
-  // });
-  //
-  // describe("combined validations", () => {
-  //   it("should check all validations and return first error found", () => {
-  //     const validations = {
-  //       minLength: 3,
-  //       maxLength: 10,
-  //       pattern: "^[A-Za-z]*$",
-  //     };
-  //
-  //     let result = validateString("testField", "abcdefghijklmn", validations);
-  //     assert.equal(result.isValid, false);
-  //     assert.equal(
-  //       result.error,
-  //       "testField exceeds the maximum length of 10 characters",
-  //     );
-  //
-  //     result = validateString("testField", "ab", validations);
-  //     assert.equal(result.isValid, false);
-  //     assert.equal(
-  //       result.error,
-  //       "testField shorter than the minimum length of 3 characters",
-  //     );
-  //
-  //     result = validateString("testField", "abc123", validations);
-  //     assert.equal(result.isValid, false);
-  //     assert.equal(result.error, "testField must be in the format ^[A-Za-z]*$");
-  //
-  //     result = validateString("testField", "abcDef", validations);
-  //     assert.equal(result.isValid, true);
-  //   });
-  // });
+  describe("minLength validation", () => {
+    it("should validate non string minLength value", () => {
+      const result = validateString("Username", 1, {
+        maxLength: 1,
+      });
+
+      assert.equal(result.isValid, false);
+      assert.equal(result.error, "Username must be a string");
+    });
+
+    it("should validate empty value", () => {
+      const result = validateString("Username", "", {
+        maxLength: 10,
+      });
+
+      assert.equal(result.isValid, false);
+      assert.equal(result.error, "Username cannot be empty");
+    });
+
+    it("should validate minimum length", () => {
+      const value = "abcde";
+      const result = validateString("Username", value, {
+        minLength: 10,
+      });
+
+      assert.equal(result.isValid, false);
+      assert.equal(
+        result.error,
+        "Username shorter than the minimum length of 10 characters",
+      );
+    });
+  });
+
+  describe("combined validations", () => {
+    it("should check all validations and return first error found", () => {
+      const validations = {
+        minLength: 3,
+        maxLength: 10,
+        pattern: "^[A-Za-z]*$",
+      };
+
+      let result = validateString("testField", "abcdefghijklmn", validations);
+      assert.equal(result.isValid, false);
+      assert.equal(
+        result.error,
+        "testField exceeds the maximum length of 10 characters",
+      );
+
+      result = validateString("testField", "ab", validations);
+      assert.equal(result.isValid, false);
+      assert.equal(
+        result.error,
+        "testField shorter than the minimum length of 3 characters",
+      );
+
+      result = validateString("testField", "abc123", validations);
+      assert.equal(result.isValid, false);
+      assert.equal(result.error, "testField must be in the format ^[A-Za-z]*$");
+
+      result = validateString("testField", "abcDef", validations);
+      assert.equal(result.isValid, true);
+    });
+  });
 });
