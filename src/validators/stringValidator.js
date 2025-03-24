2cec3316667f243fb24daefa1f919f1165ff0676
Tej Powar <tej@powtech.co.uk>
2025-03-24 11:57:15 +0000

    add minlength implementation for string validation

--- src/validators/stringValidator.js
+++ src/validators/stringValidator.js
@@ -1,4 +1,4 @@
-export const stringValidator = (fieldName, value, validations) => {
+export const validateString = (fieldName, value, validations) => {
   if (
     validations.maxLength !== undefined &&
     value.length > validations.maxLength
@@ -8,4 +8,18 @@ export const stringValidator = (fieldName, value, validations) => {
       error: `${fieldName} exceeds the maximum length of ${validations.maxLength} characters`,
     };
   }
+
+  if (
+    validations.minLength !== undefined &&
+    value.length < validations.minLength
+  ) {
+    return {
+      isValid: false,
+      error: `${fieldName} shorter than the minimum length of ${validations.minLength} characters`,
+    };
+  }
+
+  return {
+    isValid: true,
+  };
 };
--- src/validators/stringValidator.test.js
+++ src/validators/stringValidator.test.js
@@ -1,11 +1,11 @@
-import { stringValidator } from "./stringValidator.js";
+import { validateString } from "./stringValidator.js";
 import { describe, it } from "node:test";
 import { assert } from "../common/assert.js";
 
-describe("stringValidator", () => {
+describe("validateString", () => {
   describe("maxLength validation", () => {
     it("should return error when string exceeds maxLength", () => {
-      const result = stringValidator("testField", "abcdefghijklmn", {
+      const result = validateString("testField", "abcdefghijklmn", {
         maxLength: 5,
       });
       assert.equal(result.isValid, false);
@@ -13,6 +13,37 @@ describe("stringValidator", () => {
         result.error,
         "testField exceeds the maximum length of 5 characters",
       );
+    });
+    it("should pass when string is equal to maxLength", () => {
+      const result = validateString("testField", "abcde", { maxLength: 5 });
+      assert.equal(result.isValid, true);
+    });
+
+    it("should pass when string is less than maxLength", () => {
+      const result = validateString("testField", "abc", { maxLength: 5 });
+      assert.equal(result.isValid, true);
+    });
+  });
+
+  describe("minLength validation", () => {
+    it("should return error when string is less than minLength", () => {
+      const result = validateString("testField", "abc", {
+        minLength: 5,
+      });
+      assert.equal(result.isValid, false);
+      assert.equal(
+        result.error,
+        "testField shorter than the minimum length of 5 characters",
+      );
+    });
+    it("should pass when string is equal to minLength", () => {
+      const result = validateString("testField", "abcde", { minLength: 5 });
+      assert.equal(result.isValid, true);
+    });
+
+    it("should pass when string is greater than maxLength", () => {
+      const result = validateString("testField", "abcdef", { minLength: 5 });
+      assert.equal(result.isValid, true);
     });
   });
 });
