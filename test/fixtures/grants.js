1ebe1ecba2f5e9b642aabe2744048f1d4efdf2d7
sripappula <sripappula@gmail.com>
2025-04-16 10:59:18 +0100

    Adding steel thread grant integration tests (#58)
    * Adding steel thread grant integration tests
    
    * Adding steel thread grant integration tests
    
    ---------
    
    Co-authored-by: Srikanth Pappula <srikanth.pappula@esynergy.co.uk>

--- test/fixtures/grants.js
+++ test/fixtures/grants.js
@@ -47,3 +47,90 @@ export const grant2 = {
     },
   },
 };
+
+export const grant3 = {
+  code: "test-code-3",
+  metadata: {
+    description: "test description 3",
+    startDate: "2100-01-01T00:00:00.000Z",
+  },
+  actions: [
+    {
+      name: "action3",
+      method: "GET",
+      url: "http://example.com",
+    },
+  ],
+  questions: {
+    $schema: "https://json-schema.org/draft/2020-12/schema",
+    type: "object",
+    additionalProperties: false,
+    required: [
+      "scheme",
+      "year",
+      "hasCheckedLandIsUpToDate",
+      "actionApplications",
+    ],
+    properties: {
+      scheme: {
+        type: "string",
+        title: "Defra scheme name",
+      },
+      year: {
+        type: "integer",
+        title: "Scheme year",
+      },
+      hasCheckedLandIsUpToDate: {
+        type: "boolean",
+        title: "Has the applicant checked that their land data is up to date?",
+      },
+      actionApplications: {
+        type: "array",
+        title: "Details of the actions applied for",
+        minItems: 1,
+        items: {
+          $ref: "#/$defs/ActionApplication",
+        },
+      },
+    },
+    $defs: {
+      ActionApplication: {
+        type: "object",
+        additionalProperties: false,
+        required: ["parcelId", "sheetId", "code", "appliedFor"],
+        properties: {
+          parcelId: {
+            type: "string",
+            title: "Land parcel ID, unique within a OS Sheet ID",
+            pattern: "^\\d+$",
+          },
+          sheetId: {
+            type: "string",
+            title: "OS Sheet ID",
+            pattern: "[A-Z]{2}[0-9]{4}",
+          },
+          code: {
+            type: "string",
+            title: "Code for the action to be taken on the land parcel",
+          },
+          appliedFor: {
+            type: "object",
+            additionalProperties: false,
+            required: ["unit", "quantity"],
+            properties: {
+              unit: {
+                type: "string",
+                title: "Unit of measurement",
+                enum: ["kg", "m", "100m", "count", "ha", "m2"],
+              },
+              quantity: {
+                type: "number",
+                title: "Quantity of units applied for",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
--- test/grant-api.test.js
+++ test/grant-api.test.js
@@ -11,7 +11,7 @@ import { env } from "node:process";
 import http from "node:http";
 import Wreck from "@hapi/wreck";
 import { MongoClient } from "mongodb";
-import { grant1, grant2 } from "./fixtures/grants.js";
+import { grant1, grant2, grant3 } from "./fixtures/grants.js";
 import Joi from "joi";
 
 let grants;
@@ -89,14 +89,14 @@ describe("GET /grants", () => {
   });
 
   it("finds grants", async () => {
-    await grants.insertMany([{ ...grant1 }, { ...grant2 }]);
+    await grants.insertMany([{ ...grant1 }, { ...grant2 }, { ...grant3 }]);
 
     const response = await Wreck.get(`${env.API_URL}/grants`, {
       json: true,
     });
 
     expect(response.res.statusCode).toEqual(200);
-    expect(response.payload).toEqual([grant1, grant2]);
+    expect(response.payload).toEqual([grant1, grant2, grant3]);
   });
 });
 
@@ -368,6 +368,241 @@ describe("POST /grants/{code}/applications", () => {
       statusCode: 409,
       error: "Conflict",
       message: 'Application with clientRef "12345" already exists',
+    });
+  });
+  it("success add application", async () => {
+    await Wreck.post(`${env.API_URL}/grants`, {
+      json: true,
+      payload: grant3,
+    });
+
+    const response = await Wreck.post(
+      `${env.API_URL}/grants/test-code-3/applications`,
+      {
+        json: true,
+        payload: {
+          metadata: {
+            clientRef: "12345",
+            submittedAt: new Date(),
+            sbi: "1234567890",
+            frn: "1234567890",
+            crn: "1234567890",
+            defraId: "1234567890",
+          },
+          answers: {
+            scheme: "SFI",
+            year: 2025,
+            hasCheckedLandIsUpToDate: true,
+            actionApplications: [
+              {
+                parcelId: "9238",
+                sheetId: "SX0679",
+                code: "CSAM1",
+                appliedFor: {
+                  unit: "ha",
+                  quantity: 20.23,
+                },
+              },
+            ],
+          },
+        },
+      },
+    );
+
+    expect(response.res.statusCode).toEqual(201);
+    expect(response.payload).toEqual({
+      clientRef: "12345",
+    });
+  });
+  it("responds with 400 when the application send invalid unit value", async () => {
+    await Wreck.post(`${env.API_URL}/grants`, {
+      json: true,
+      payload: grant3,
+    });
+
+    let response;
+    try {
+      await Wreck.post(`${env.API_URL}/grants/test-code-3/applications`, {
+        json: true,
+        payload: {
+          metadata: {
+            clientRef: "12345",
+            submittedAt: new Date(),
+            sbi: "1234567890",
+            frn: "1234567890",
+            crn: "1234567890",
+            defraId: "1234567890",
+          },
+          answers: {
+            scheme: "SFI",
+            year: 2025,
+            hasCheckedLandIsUpToDate: true,
+            actionApplications: [
+              {
+                parcelId: "123456",
+                sheetId: "AB1234",
+                code: "WEEDING",
+                appliedFor: {
+                  unit: "liters",
+                  quantity: 2,
+                },
+              },
+            ],
+          },
+        },
+      });
+    } catch (err) {
+      response = err.data.payload;
+    }
+
+    expect(response).toEqual({
+      statusCode: 400,
+      error: "Bad Request",
+      message:
+        'Application with clientRef "12345" has invalid answers: data/actionApplications/0/appliedFor/unit must be equal to one of the allowed values',
+    });
+  });
+  it("responds with 400 when the application send missing code value", async () => {
+    await Wreck.post(`${env.API_URL}/grants`, {
+      json: true,
+      payload: grant3,
+    });
+
+    let response;
+    try {
+      await Wreck.post(`${env.API_URL}/grants/test-code-3/applications`, {
+        json: true,
+        payload: {
+          metadata: {
+            clientRef: "12345",
+            submittedAt: new Date(),
+            sbi: "1234567890",
+            frn: "1234567890",
+            crn: "1234567890",
+            defraId: "1234567890",
+          },
+          answers: {
+            scheme: "SFI",
+            year: 2025,
+            hasCheckedLandIsUpToDate: true,
+            actionApplications: [
+              {
+                parcelId: "123456",
+                sheetId: "AB1234",
+                appliedFor: {
+                  unit: "count",
+                  quantity: 1,
+                },
+              },
+            ],
+          },
+        },
+      });
+    } catch (err) {
+      response = err.data.payload;
+    }
+
+    expect(response).toEqual({
+      statusCode: 400,
+      error: "Bad Request",
+      message:
+        "Application with clientRef \"12345\" has invalid answers: data/actionApplications/0 must have required property 'code'",
+    });
+  });
+  it("responds with 400 when the application send invalid sheet value", async () => {
+    await Wreck.post(`${env.API_URL}/grants`, {
+      json: true,
+      payload: grant3,
+    });
+
+    let response;
+    try {
+      await Wreck.post(`${env.API_URL}/grants/test-code-3/applications`, {
+        json: true,
+        payload: {
+          metadata: {
+            clientRef: "12345",
+            submittedAt: new Date(),
+            sbi: "1234567890",
+            frn: "1234567890",
+            crn: "1234567890",
+            defraId: "1234567890",
+          },
+          answers: {
+            scheme: "SFI",
+            year: 2025,
+            hasCheckedLandIsUpToDate: true,
+            actionApplications: [
+              {
+                parcelId: "123456",
+                sheetId: "A1234",
+                code: "TREE_PLANTING",
+                appliedFor: {
+                  unit: "ha",
+                  quantity: 3,
+                },
+              },
+            ],
+          },
+        },
+      });
+    } catch (err) {
+      response = err.data.payload;
+    }
+
+    expect(response).toEqual({
+      statusCode: 400,
+      error: "Bad Request",
+      message:
+        'Application with clientRef "12345" has invalid answers: data/actionApplications/0/sheetId must match pattern "[A-Z]{2}[0-9]{4}"',
+    });
+  });
+  it("responds with 400 when the application send invalid parcel Id value", async () => {
+    await Wreck.post(`${env.API_URL}/grants`, {
+      json: true,
+      payload: grant3,
+    });
+
+    let response;
+    try {
+      await Wreck.post(`${env.API_URL}/grants/test-code-3/applications`, {
+        json: true,
+        payload: {
+          metadata: {
+            clientRef: "12345",
+            submittedAt: new Date(),
+            sbi: "1234567890",
+            frn: "1234567890",
+            crn: "1234567890",
+            defraId: "1234567890",
+          },
+          answers: {
+            scheme: "SFI",
+            year: 2025,
+            hasCheckedLandIsUpToDate: true,
+            actionApplications: [
+              {
+                parcelId: "abc123",
+                sheetId: "AB1234",
+                code: "TREE_PLANTING",
+                appliedFor: {
+                  unit: "ha",
+                  quantity: 10,
+                },
+              },
+            ],
+          },
+        },
+      });
+    } catch (err) {
+      response = err.data.payload;
+    }
+
+    expect(response).toEqual({
+      statusCode: 400,
+      error: "Bad Request",
+      message:
+        'Application with clientRef "12345" has invalid answers: data/actionApplications/0/parcelId must match pattern "^\\d+$"',
     });
   });
 });
