ee3051cc24785b93445d7d5ac2a94bd638130ac7
Tej Powar <tej@powtech.co.uk>
2025-05-13 13:25:55 +0100

    Fut 427 gas application approved (#78)
    https://eaflood.atlassian.net/browse/FUT-427
    
    Create publish event for the grant application approved. Also setup localstack with topics for case_stage_updated and queue for case_stage_updates

--- compose/start-localstack.sh
+++ compose/start-localstack.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-# SNS topic
+# SNS topic - grant application created
 aws --endpoint-url=http://localhost:4566 sns create-topic --name grant-application-created
 
 # SQS queues
@@ -18,3 +18,23 @@ aws --endpoint-url=http://localhost:4566 sqs set-queue-attributes \
 # Subscribe queue to topic
 aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn arn:aws:sns:eu-west-2:000000000000:grant-application-created \
 --protocol sqs --notification-endpoint arn:aws:sqs:eu-west-2:000000000000:grant-application-created
+
+# SNS topic - grant application approved
+aws --endpoint-url=http://localhost:4566 sns create-topic --name grant_application_approved
+
+# SNS topic - case stage updated
+aws --endpoint-url=http://localhost:4566 sns create-topic --name case_stage_updated
+
+# SQS queues = case stage updates
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name case_stage_updates
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name case_stage_updates-dead-letter-queue
+
+# Configure dead letter queue
+aws --endpoint-url=http://localhost:4566 sqs set-queue-attributes \
+--queue-url http://sqs.eu-west-2.127.0.0.1:4566/000000000000/case_stage_updates \
+--attributes '{ "RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:case_stage_updates-dead-letter-queue\",\"maxReceiveCount\":\"1\"}" }'
+
+# Subscribe queue to topic
+aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn arn:aws:sns:eu-west-2:000000000000:case_stage_updated \
+--protocol sqs --notification-endpoint arn:aws:sqs:eu-west-2:000000000000:case_stage_updates \
+--attributes '{ "RawMessageDelivery": "true"}'
---
+++ src/events/case-stage-updates-queue-event-handler.js
@@ -0,0 +1,37 @@
+import * as sns from "../common/sns.js";
+import * as grantService from "../grants/grant-service.js";
+import { config } from "../common/config.js";
+import { randomUUID } from "crypto";
+
+const caseStageUpdatesQueueEventHandler = (server) => async (message) => {
+  server.logger.info({
+    message: "Received SQS message",
+    body: message.MessageId,
+  });
+
+  const messageBody = JSON.parse(message.Body);
+
+  const application = await grantService.findApplicationByClientRef(
+    messageBody.data.clientRef,
+  );
+
+  if (messageBody.data.currentStage === "contract") {
+    const event = {
+      id: randomUUID(),
+      source: config.serviceName,
+      specVersion: "1.0",
+      type: `cloud.defra.${config.env}.${config.serviceName}.application.approved`,
+      datacontenttype: "application/json",
+      data: application,
+    };
+
+    await sns.publish(config.grantApplicationApprovedTopic, event);
+
+    server.logger.info({
+      message: `Grant approval event sent: ${event.id}`,
+      body: event,
+    });
+  }
+};
+
+export { caseStageUpdatesQueueEventHandler };
