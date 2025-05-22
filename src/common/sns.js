cd779cb33c10252491d7c038c47c78acec9f0e6a
julian <juliankimmings@gmail.com>
2025-05-22 14:59:06 +0100

    nope

--- src/common/sns.js
+++ src/common/sns.js
@@ -1,7 +1,5 @@
 import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
 import { config } from "./config.js";
-import { randomUUID } from "crypto";
-import { getTraceId } from "@defra/hapi-tracing";
 
 export const snsClient = new SNSClient({
   region: config.region,
@@ -15,19 +13,9 @@ export const snsClient = new SNSClient({
  * @returns
  */
 export const publish = async (topicArn, message) => {
-  const event = {
-    id: randomUUID(),
-    source: config.serviceName,
-    specVersion: "1.0",
-    type: `cloud.defra.${config.env}.${config.serviceName}.application.created`,
-    datacontenttype: "application/json",
-    data: message,
-    traceparent: getTraceId(),
-  };
-
   return snsClient.send(
     new PublishCommand({
-      Message: JSON.stringify(event),
+      Message: JSON.stringify(message),
       TopicArn: topicArn,
     }),
   );
