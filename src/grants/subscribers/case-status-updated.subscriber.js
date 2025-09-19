05216cd4a6f10ab0255538d6afb1ceccfefcaf9f
julian <juliankimmings@gmail.com>
2025-09-19 11:01:30 +0100

    use constant

--- src/grants/subscribers/case-status-updated.subscriber.js
+++ src/grants/subscribers/case-status-updated.subscriber.js
@@ -1,16 +1,15 @@
 import { config } from "../../common/config.js";
 import { SqsSubscriber } from "../../common/sqs-subscriber.js";
-// eslint-disable-next-line import-x/no-restricted-paths
-import { ApplicationStatus } from "../models/application.js";
-
 import { approveApplicationUseCase } from "../use-cases/approve-application.use-case.js";
+
+const APPROVED = "APPROVED";
 
 export const caseStatusUpdatedSubscriber = new SqsSubscriber({
   queueUrl: config.sqs.updateStatusQueueUrl,
   async onMessage(message) {
     const { data } = message;
 
-    if (data.currentStatus === ApplicationStatus.Approved) {
+    if (data.currentStatus === APPROVED) {
       await approveApplicationUseCase(data);
     }
   },
