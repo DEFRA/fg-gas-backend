fa2d73ff8c0037a9bad57048896d48886fafe4dc
julian <juliankimmings@gmail.com>
2025-09-19 16:22:22 +0100

    log case status updated

--- src/grants/subscribers/case-status-updated.subscriber.js
+++ src/grants/subscribers/case-status-updated.subscriber.js
@@ -1,4 +1,5 @@
 import { config } from "../../common/config.js";
+import { logger } from "../../common/logger.js";
 import { SqsSubscriber } from "../../common/sqs-subscriber.js";
 import { approveApplicationUseCase } from "../use-cases/approve-application.use-case.js";
 
@@ -7,6 +8,9 @@ const APPROVED = "APPROVED";
 export const caseStatusUpdatedSubscriber = new SqsSubscriber({
   queueUrl: config.sqs.updateStatusQueueUrl,
   async onMessage(message) {
+    logger.info(
+      `Case Status Updated Subscriber.  Status: ${message.data.currentStatus}`,
+    );
     const { data } = message;
 
     if (data.currentStatus === APPROVED) {
