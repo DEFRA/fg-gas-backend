f01090ba2317e7cff4812d0c6bac69e28a574909
Julian Kimmings <35488092+kimmiju@users.noreply.github.com>
2025-09-25 09:46:37 +0100

    fix: FGP-523 - add missing validation for url params (#207)
    * add missing validation for url params

--- src/grants/routes/application-status.route.js
+++ src/grants/routes/application-status.route.js
@@ -1,3 +1,6 @@
+import Joi from "joi";
+import { clientRef } from "../schemas/application/metadata/client-ref.js";
+import { code } from "../schemas/grant/code.js";
 import { applicationStatusResponseSchema } from "../schemas/responses/application-status-response.schema.js";
 import { getApplicationStatusUseCase } from "../use-cases/get-application-status.use-case.js";
 
@@ -7,6 +10,12 @@ export const applicationStatusRoute = {
   options: {
     description: "Get application status",
     tags: ["api"],
+    validate: {
+      params: Joi.object({
+        code,
+        clientRef,
+      }),
+    },
     response: {
       schema: applicationStatusResponseSchema,
     },
