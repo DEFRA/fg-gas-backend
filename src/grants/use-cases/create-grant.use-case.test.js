2826c38609894ffa455bff7d453fe4e0414ee296
Aditya Kolnati <aditya.kolnati@gmail.com>
2025-10-27 09:34:52 +0000

    implement lifecycle transitions latest (#254)
    * Dump of ideas
    
    * Dump of ideas
    
    * FGP-199 gas restructure grant definition to include phases and stages (#238)
    
    * Add migrations for grant phase, stage, status
    
    * Add support for phases to Grant model
    
    * Update Grant model and repository to replace questions with phases
    
    * Refactor grant and application models to introduce `phases`
    
    * Refactor `Application` model and tests to use phase
    
    * Add `createTestApplication` helper for testing `Application` model
    
    * Refactor `publishCreateNewCase` tests to use `createTestApplication` helper
    
    * Replace `questions` with `phases` across grant schema, use case, and tests.
    
    * Refactor grant schema, use cases, and tests to replace `questions` with `phases`
    
    * Replace `questions` with `phases` in find grants route tests and response schema
    
    * Replace `questions` with `phases` in replace grant route tests
    
    * Refactor tests to use `createTestGrant` and `createTestApplication` helpers, replace `questions` with `phases`
    
    * Add unit tests for `Grant` model
    
    * Refactor integration and application status tests to replace `questions` with `phases`
    
    * Refactor simple integration tests to replace `questions` with `phases`
    
    * Refactor application status tests to include `ApplicationPhase`, `ApplicationStage`, and `ApplicationStatus`
    
    * Refactor submit application tests to replace `questions` with `phases`
    
    * Refactor application-service integration tests to replace `questions` with `phases`
    
    * Refactor grant service integration tests to replace `questions` with `phases`
    
    * Fix submit application tests to update status
    
    * Migrate application `answers` field to `phases` in database schema
    
    * Remove unused methods
    
    ---------
    
    Co-authored-by: Justin Ramel <justin.ramel@gmail.com>
    
    * Add PR checks to PRs targeting the dev branch (#239)
    
    * Update frps private beta grant structure (#248)
    
    * status transition for grants with phases and stages
    
    * changed the file type to be consistent with current setup
    
    * fixed the file reference issue
    
    * fixed the failing unit tests
    
    * updated test to improve code coverage added fix to const
    
    * FGP-196: outbox pattern (#244)
    
    * update compose file for replica sets
    
    * add outbox migration
    
    * outbox model and withTransaction wrapper
    
    * update es lint to allow access to repositories
    
    * fix potential null phases
    
    * outbox repos/models and testing with approve application use case
    
    * pulls out the expired events so we’re not immediately failing expired events
    
    * update the code to fix the failing integration tests
    
    * integrate outbox pattern into status transition
    
    * updated code to reflect cr comments and other changes
    
    ---------
    
    Co-authored-by: Martin Smith <martin@smitek.co.uk>
    Co-authored-by: Donatas Bacius <bacius@gmail.com>
    Co-authored-by: Justin Ramel <justin.ramel@gmail.com>
    Co-authored-by: Aditya Kolnati <adityakolnati@adityas.mac.pro.m2.max.chip.lan>
    Co-authored-by: Julian Kimmings <35488092+kimmiju@users.noreply.github.com>

--- src/grants/use-cases/create-grant.use-case.test.js
+++ src/grants/use-cases/create-grant.use-case.test.js
@@ -47,4 +47,92 @@ describe("createGrantUseCase", () => {
       }),
     );
   });
+
+  it("creates a grant with externalStatusMap", async () => {
+    const grant = await createGrantUseCase({
+      code: "test-grant",
+      metadata: {
+        description: "Test Grant Description",
+        startDate: "2023-01-01T00:00:00Z",
+      },
+      actions: [
+        { name: "action1", method: "POST", url: "http://example.com/action1" },
+      ],
+      questions: {
+        $schema: "https://json-schema.org/draft/2020-12/schema",
+        type: "object",
+      },
+      externalStatusMap: {
+        phases: [
+          {
+            code: "PRE_AWARD",
+            stages: [
+              {
+                code: "REVIEW",
+                statuses: [
+                  {
+                    code: "IN_PROGRESS",
+                    source: "CW",
+                    mappedTo: "IN_PROGRESS",
+                  },
+                  {
+                    code: "APPROVED",
+                    source: "CW",
+                    mappedTo: "APPROVED",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    });
+
+    expect(save).toHaveBeenCalledWith(grant);
+
+    expect(grant).toStrictEqual(
+      new Grant({
+        code: "test-grant",
+        metadata: {
+          description: "Test Grant Description",
+          startDate: "2023-01-01T00:00:00Z",
+        },
+        actions: [
+          {
+            name: "action1",
+            method: "POST",
+            url: "http://example.com/action1",
+          },
+        ],
+        questions: {
+          $schema: "https://json-schema.org/draft/2020-12/schema",
+          type: "object",
+        },
+        externalStatusMap: {
+          phases: [
+            {
+              code: "PRE_AWARD",
+              stages: [
+                {
+                  code: "REVIEW",
+                  statuses: [
+                    {
+                      code: "IN_PROGRESS",
+                      source: "CW",
+                      mappedTo: "IN_PROGRESS",
+                    },
+                    {
+                      code: "APPROVED",
+                      source: "CW",
+                      mappedTo: "APPROVED",
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      }),
+    );
+  });
 });
