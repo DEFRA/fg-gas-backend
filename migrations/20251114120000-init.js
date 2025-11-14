67de6186b9c16cc1ffa6ac4945ec871b04093bef
Donatas Bacius <bacius@gmail.com>
2025-11-14 14:09:57 +0000

    Compact migrations
    https://eaflood.atlassian.net/browse/FGP-706

---
+++ migrations/20251114120000-init.js
@@ -0,0 +1,32 @@
+export const up = async (db) => {
+  const grants = db.collection("grants");
+  await grants.drop().catch(() => {});
+  await grants.createIndex({ code: 1 }, { unique: true });
+
+  const applications = db.collection("applications");
+  await applications.drop().catch(() => {});
+  await applications.createIndex({ clientRef: 1, code: 1 }, { unique: true });
+
+  const outbox = db.collection("outbox");
+  await outbox.drop().catch(() => {});
+  await outbox.createIndex({
+    status: 1,
+    claimedBy: 1,
+    completionAttempts: 1,
+    publicationDate: 1,
+  });
+  await outbox.createIndex({ claimExpiresAt: 1 });
+  await outbox.createIndex({ status: 1, completionAttempts: 1 });
+
+  const inbox = db.collection("inbox");
+  await inbox.drop().catch(() => {});
+  await inbox.createIndex({
+    status: 1,
+    claimedBy: 1,
+    completionAttempts: 1,
+    publicationDate: 1,
+  });
+  await inbox.createIndex({ claimExpiresAt: 1 });
+  await inbox.createIndex({ messageId: 1 });
+  await inbox.createIndex({ status: 1, completionAttempts: 1 });
+};
