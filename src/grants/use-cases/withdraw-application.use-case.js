7132d8a8f681a6811ee9a4d68955346fe901c384
julian <juliankimmings@gmail.com>
2025-12-08 14:01:13 +0000

    rm console logs

--- src/grants/use-cases/withdraw-application.use-case.js
+++ src/grants/use-cases/withdraw-application.use-case.js
@@ -2,13 +2,13 @@ import { config } from "../../common/config.js";
 import { UpdateCaseStatusCommand } from "../commands/update-case-status.command.js";
 import { ApplicationStatusUpdatedEvent } from "../events/application-status-updated.event.js";
 import { UpdateAgreementStatusCommand } from "../events/update-agreement-status.command.js";
+import { AgreementServiceStatus } from "../models/agreement.js";
 import { Outbox } from "../models/outbox.js";
 import {
   findByClientRefAndCode,
   update,
 } from "../repositories/application.repository.js";
 import { insertMany } from "../repositories/outbox.repository.js";
-import { AgreementServiceStatus } from "../models/agreement.js";
 
 export const withdrawApplicationUseCase = async (command, session) => {
   const { clientRef, code } = command;
@@ -28,20 +28,18 @@ export const withdrawApplicationUseCase = async (command, session) => {
   const outboxObjects = [];
 
   if (agreement) {
-    console.log("withdraw application use case", agreement)
     // create a withdraw agreement command for Agreement Service
     const updateAgreementStatusCommand = new UpdateAgreementStatusCommand({
       clientRef,
       code,
-      status: AgreementServiceStatus.Withdrawn
+      status: AgreementServiceStatus.Withdrawn,
     });
 
-    console.log(config.sns.updateAgreementStatusTopicArn, config.sns);
     outboxObjects.push(
       new Outbox({
         event: updateAgreementStatusCommand,
-        target: config.sns.updateAgreementStatusTopicArn
-      })
+        target: config.sns.updateAgreementStatusTopicArn,
+      }),
     );
   } else {
     // if we have no agreement we withdraw the application and notify Case Working...
