93eac765e0a5f31d16d872ba6d4742c9b596a0a2
Tejwant Powar <tejwant.powar@equalexperts.com>
2025-12-09 16:47:16 +0000

    update migration

--- migrations/20251208101900-update-validfrom-transition-processes.js
+++ migrations/20251208101900-update-validfrom-transition-processes.js
@@ -2,42 +2,29 @@ export const up = async (db) => {
   const grants = await db.collection("grants").find({}).toArray();
 
   for (const grant of grants) {
-    if (grant.phases) {
-      for (const phase of grant.phases) {
-        if (phase.stages) {
-          for (const stage of phase.stages) {
-            if (stage.statuses) {
-              stage.statuses = stage.statuses.map((status) => {
-                if (status.validFrom && Array.isArray(status.validFrom)) {
-                  // Get the processes from the status level
-                  const statusLevelProcesses = status.processes || [];
-
-                  const newValidFrom = status.validFrom.map((fromCode) => {
-                    // APPLICATION_REJECTED should always have empty processes
-                    if (fromCode === "APPLICATION_REJECTED") {
-                      return {
-                        code: fromCode,
-                        processes: [],
-                      };
-                    }
-
-                    return {
-                      code: fromCode,
-                      processes: statusLevelProcesses,
-                    };
-                  });
-
-                  return {
-                    code: status.code,
-                    validFrom: newValidFrom,
-                  };
-                }
-
-                return status;
-              });
+    for (const phase of grant.phases) {
+      for (const stage of phase.stages) {
+        stage.statuses = stage.statuses.map((status) => {
+          const newValidFrom = (status.validFrom || []).map((fromCode) => {
+            // APPLICATION_REJECTED should always have empty processes
+            if (fromCode === "APPLICATION_REJECTED") {
+              return {
+                code: fromCode,
+                processes: [],
+              };
             }
-          }
-        }
+
+            return {
+              code: fromCode,
+              processes: status.processes || [],
+            };
+          });
+
+          return {
+            code: status.code,
+            validFrom: newValidFrom,
+          };
+        });
       }
     }
 
