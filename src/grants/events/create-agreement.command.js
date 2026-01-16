0424919840a996611624f3da6003314d2a9cab84
Justin Ramel <justin.ramel@esynergy.co.uk>
2026-01-16 09:27:50 +0000

    Remove the temp defraId fallback

--- src/grants/events/create-agreement.command.js
+++ src/grants/events/create-agreement.command.js
@@ -2,15 +2,10 @@ import { CloudEvent } from "../../common/cloud-event.js";
 
 export class CreateAgreementCommand extends CloudEvent {
   constructor(application) {
-    const identifiers = {
-      ...application.identifiers,
-      defraId: application.metadata?.defraId ?? "defraId", // TODO: This it temporary, remove once the agreement service have removed the requirement
-    };
-
     super("agreement.create", {
       clientRef: application.clientRef,
       code: application.code,
-      identifiers,
+      identifiers: application.identifiers,
       metadata: application.metadata,
       answers: application.getAnswers(),
     });
