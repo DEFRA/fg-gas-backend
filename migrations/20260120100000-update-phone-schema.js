efb1f8b3c76b4212021ae628393a030b65307d61
Aditya Kolnati <adityakolnati@adityas.mac.pro.m2.max.chip.lan>
2026-01-20 20:53:50 +0000

    update the phone format

---
+++ migrations/20260120100000-update-phone-schema.js
@@ -0,0 +1,20 @@
+export const up = async (db) => {
+  const collection = db.collection("grants");
+  const query = { code: "frps-private-beta" };
+
+  const newPhoneSchema = {
+    type: "string",
+    pattern: "^\\+?[0-9\\s\\-()]+$",
+    description: "Phone number",
+  };
+
+  // Update phone schema from object with mobile property to simple string
+  await collection.updateOne(query, {
+    $set: {
+      "phases.$[phase].questions.$defs.Applicant.properties.business.properties.phone":
+        newPhoneSchema,
+    },
+  }, {
+    arrayFilters: [{ "phase.code": "PRE_AWARD" }],
+  });
+};
