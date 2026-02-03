145d7057bbd37fe05b97dd28400bfccbb8fbb66b
julian <juliankimmings@gmail.com>
2026-02-03 14:59:55 +0000

    update migratation for backwards compatibility

--- migrations/20260123122700-add-fifo-lock-collection.js
+++ migrations/20260123122700-add-fifo-lock-collection.js
@@ -1,6 +1,65 @@
+import { withTransaction } from "../src/common/with-transaction.js";
 export const up = async (db) => {
   const fifoLock = db.collection("fifo_locks");
   fifoLock.drop().catch(() => {});
   fifoLock.createIndex({ segregationRef: 1 }, { unique: true });
   fifoLock.createIndex({ locked: 1, segregationRef: 1, lockedAt: 1 });
+
+  await withTransaction(async (session) => {
+    await await db.collection("inbox").updateMany(
+      {},
+      [
+        {
+          $set: {
+            segregationRef: {
+              $let: {
+                vars: {
+                  ref: {
+                    $ifNull: ["$event.data.caseRef", "$event.data.clientRef"],
+                  },
+                  code: {
+                    $ifNull: [
+                      "$event.data.workflowCode",
+                      "$event.data.grantCode",
+                    ],
+                  },
+                },
+                in: { $concat: ["$$ref", "-", "$$code"] },
+              },
+            },
+          },
+        },
+      ],
+      { session },
+    );
+
+    db.collection("outbox").updateMany(
+      {},
+      [
+        {
+          $set: {
+            "event.messageGroupId": {
+              $let: {
+                vars: {
+                  ref: {
+                    $ifNull: ["$event.data.caseRef", "$event.data.clientRef"],
+                  },
+                  code: {
+                    $ifNull: [
+                      "$event.data.workflowCode",
+                      {
+                        $ifNull: ["$event.data.code", "$event.data.grantCode"],
+                      },
+                    ],
+                  },
+                },
+                in: { $concat: ["$$ref", "-", "$$code"] },
+              },
+            },
+          },
+        },
+      ],
+      { session },
+    );
+  });
 };
