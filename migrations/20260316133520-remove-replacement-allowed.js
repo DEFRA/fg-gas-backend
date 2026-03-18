b7302991aeb0420cb677969b2243aa2a0d63812d
julian <juliankimmings@gmail.com>
2026-03-18 12:42:56 +0000

    remove unused method

--- migrations/20260316133520-remove-replacement-allowed.js
+++ migrations/20260316133520-remove-replacement-allowed.js
@@ -47,14 +47,3 @@ export const up = async (db) => {
     }
   });
 };
-
-/**
- * @param db {import('mongodb').Db}
- * @param client {import('mongodb').MongoClient}
- * @returns {Promise<void>}
- */
-export const down = async (db, client) => {
-  // TODO write the statements to rollback your migration (if possible)
-  // Example:
-  // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
-};
