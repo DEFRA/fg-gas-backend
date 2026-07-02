a34da83a631703249d1131d7d7845fc51b253ae2
Justin Ramel <justin.ramel@esynergy.co.uk>
2026-07-02 11:17:42 +0100

    Fix incorrect payment pence value for WMP applications in migrations

---
+++ migrations/20260702120000-fix-wmp-payments-pence.js
@@ -0,0 +1,46 @@
+const CLIENT_REF = "wmp-2ax-726";
+const CODE = "woodland";
+const PHASE_CODE = "PHASE_PRE_AWARD";
+const BAD_PENCE = { $gt: 238769, $lt: 238770 };
+const FIXED_PENCE = 238770;
+
+export const up = async (db) => {
+  await db.collection("applications").updateOne(
+    {
+      clientRef: CLIENT_REF,
+      code: CODE,
+      "phases.answers.totalAgreementPaymentPence": BAD_PENCE,
+    },
+    {
+      $set: {
+        "phases.$[phase].answers.totalAgreementPaymentPence": FIXED_PENCE,
+        "phases.$[phase].answers.payments.agreement.0.agreementTotalPence":
+          FIXED_PENCE,
+      },
+    },
+    {
+      arrayFilters: [{ "phase.code": PHASE_CODE }],
+    },
+  );
+
+  await db.collection("outbox").updateOne(
+    {
+      "event.type": "cloud.defra.dev.fg-gas-backend.agreement.create",
+      "event.data.clientRef": CLIENT_REF,
+      "event.data.code": CODE,
+      "event.data.answers.totalAgreementPaymentPence": BAD_PENCE,
+    },
+    {
+      $set: {
+        "event.data.answers.totalAgreementPaymentPence": FIXED_PENCE,
+        "event.data.answers.payments.agreement.0.agreementTotalPence":
+          FIXED_PENCE,
+        status: "PUBLISHED",
+        completionDate: null,
+        claimedAt: null,
+        claimedBy: null,
+        claimExpiresAt: null,
+      },
+    },
+  );
+};
