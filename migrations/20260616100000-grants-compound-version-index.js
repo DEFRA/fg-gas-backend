cd621ee2f575ef424b7837bbff32edd9a74c3067
Aditya Kolnati <adityakolnati@Adityas-Mac-Pro-M2-Max-Chip.local>
2026-07-02 14:36:10 +0100

    consolidate migration scripts

--- migrations/20260616100000-grants-compound-version-index.js
+++
@@ -1,5 +0,0 @@
-export const up = async (db) => {
-  const grants = db.collection("grants");
-  await grants.createIndex({ code: 1, version: 1 }, { unique: true });
-  await grants.dropIndex("code_1");
-};
--- migrations/20260618100000-applications-original-config-version.js
+++
@@ -1,8 +0,0 @@
-export const up = async (db) => {
-  await db
-    .collection("applications")
-    .updateMany(
-      { originalConfigVersion: { $exists: false } },
-      { $set: { originalConfigVersion: null, currentConfigVersion: null } },
-    );
-};
